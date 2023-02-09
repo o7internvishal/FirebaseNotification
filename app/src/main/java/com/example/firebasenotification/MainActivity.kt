5cdc915bff6e0827ca69deab8f0aeefd05ba65e0
o7internvishal <intern162@o7tec.com>
2023-02-09 12:56:18 +0530

    commit
    1)Foreground Notification
    2)Add channel

--- app/src/main/java/com/example/firebasenotification/MainActivity.kt
+++ app/src/main/java/com/example/firebasenotification/MainActivity.kt
@@ -1,9 +1,12 @@
 package com.example.firebasenotification
 
 
-import android.annotation.SuppressLint
-import android.content.ContentValues.TAG
-import android.content.SharedPreferences
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
 import android.content.pm.PackageManager
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
@@ -12,58 +15,70 @@ import android.util.Log
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.content.ContextCompat
+import androidx.core.content.getSystemService
 import com.google.android.gms.tasks.OnCompleteListener
-import com.google.android.gms.tasks.Task
-import com.google.firebase.FirebaseApp
-import com.google.firebase.installations.FirebaseInstallations
 import com.google.firebase.messaging.FirebaseMessaging
-import javax.inject.Inject
-
 
 class MainActivity : AppCompatActivity() {
 
     private val TAG = this.javaClass.simpleName
+    lateinit var notificationManager: NotificationManager
+    lateinit var notificationChannel: NotificationChannel
+    lateinit var builder:Notification.Builder
+    private val channelID="apps.notification"
+    private val channelDescription="Test Notification"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        notificationManager=getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
 
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
 
-// Declare the launcher at the top of your Activity/Fragment:
+            val pendingIntent: PendingIntent =
+                Intent(this, MyFirebaseMessagingServices::class.java).let { notificationIntent ->
+                    PendingIntent.getActivity(
+                        this, 0, notificationIntent,
+                        PendingIntent.FLAG_IMMUTABLE
+                    )
+                }
+
+//            val notification: Notification = Notification.Builder(this)
+//                .setContentTitle(getText(R.string.notification_title))
+//                .setContentText(getText(R.string.notification_message))
+//                .setSmallIcon(R.drawable.icon)
+//                .setContentIntent(pendingIntent)
+//                .setTicker(getText(R.string.ticker_text))
+//                .build()
+
+        }
+
         val requestPermissionLauncher = registerForActivityResult(
             ActivityResultContracts.RequestPermission()
         ) { isGranted: Boolean ->
             if (isGranted) {
-//                Firebase.messaging.isAutoInitEnabled : true
-//                setAnalyticsCollectionEnabled(true);
-
-                // FCM SDK (and your app) can post notifications.
             } else {
 
             }
         }
 
-        fun askNotificationPermission() {
-            // This is only necessary for API level >= 33 (TIRAMISU)
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
-                if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.POST_NOTIFICATIONS) ==
-                    PackageManager.PERMISSION_GRANTED
-                ) {
-//                    Firebase.messaging.isAutoInitEnabled : true
-//                    setAnalyticsCollectionEnabled(true);
-                    // FCM SDK (and your app) can post notifications.
-                } else if (shouldShowRequestPermissionRationale(android.Manifest.permission.POST_NOTIFICATIONS)) {
-                    // TODO: display an educational UI explaining to the user the features that will be enabled
-                    //       by them granting the POST_NOTIFICATION permission. This UI should provide the user
-                    //       "OK" and "No thanks" buttons. If the user selects "OK," directly request the permission.
-                    //       If the user selects "No thanks," allow the user to continue without notifications.
-                } else {
-                    // Directly ask for the permission
-                    requestPermissionLauncher.launch(android.Manifest.permission.POST_NOTIFICATIONS)
-                }
-            }
-
-        }
+//        fun askNotificationPermission() {
+//            // This is only necessary for API level >= 33 (TIRAMISU)
+//            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+//                if (ContextCompat.checkSelfPermission(
+//                        this, android.Manifest.permission.POST_NOTIFICATIONS
+//                    ) == PackageManager.PERMISSION_GRANTED
+//                ) {
+////                    Firebase.messaging.isAutoInitEnabled : true
+////                    setAnalyticsCollectionEnabled(true);
+//                    // FCM SDK (and your app) can post notifications.
+//                } else if (shouldShowRequestPermissionRationale(android.Manifest.permission.POST_NOTIFICATIONS)) {
+//                } else {
+//                    requestPermissionLauncher.launch(android.Manifest.permission.POST_NOTIFICATIONS)
+//                }
+//            }
+//
+//        }
         FirebaseMessaging.getInstance().token.addOnCompleteListener(OnCompleteListener { task ->
             if (!task.isSuccessful) {
                 Log.w(TAG, "Fetching FCM registration token failed", task.exception)
--- app/src/main/java/com/example/firebasenotification/MyFirebaseMessagingServices.kt
+++ app/src/main/java/com/example/firebasenotification/MyFirebaseMessagingServices.kt
@@ -1,60 +1,62 @@
 package com.example.firebasenotification
 
-
-import android.annotation.SuppressLint
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.app.PendingIntent
 import android.content.ContentValues.TAG
-import android.content.Context
 import android.content.Intent
-import android.media.RingtoneManager
 import android.os.Build
 import android.util.Log
-import android.widget.Toast
 import androidx.core.app.NotificationCompat
-import androidx.work.OneTimeWorkRequest
-import androidx.work.WorkManager
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
-
-
 class MyFirebaseMessagingServices : FirebaseMessagingService() {
-    /**
-     * Called if the FCM registration token is updated. This may occur if the security of
-     * the previous token had been compromised. Note that this is called when the
-     * FCM registration token is initially generated so this is where you would retrieve the token.
-     */
     override fun onNewToken(token: String) {
-        Log.d(TAG,"Refreshed token:$token")
-
-        // If you want to send messages to this application instance or
-        // manage this apps subscriptions on the server side, send the
-        // FCM registration token to your app server.
+        Log.d(TAG, "Refreshed token:$token")
         sendRegistrationToServer(token)
     }
-
     private fun sendRegistrationToServer(token: String) {
         Log.d(TAG, "sendRegistrationTokenToServer($token)")
     }
+    override fun onMessageReceived(remoteMessage: RemoteMessage) {
+        Log.d(TAG, "From: ${remoteMessage.from}")
+        Log.d(TAG, "From: ${remoteMessage.data}")
+        if (remoteMessage.data.isNotEmpty()) {
+            Log.d(TAG, "Message data payload: ${remoteMessage.data}")
 
-//    override fun onNewToken(token: String) {
-//        Log.d(TAG, "Refreshed token: $token")
-//
-//        // If you want to send messages to this application instance or
-//        // manage this apps subscriptions on the server side, send the
-//        // FCM registration token to your app server.
-//        sendRegistrationToServer(token)
-//    }
-//
-//    private fun sendRegistrationToServer(token: String) {
-//
-//    }
+        }
+        val intent = Intent(this, MainActivity::class.java);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        val pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_ONE_SHOT);
+        val channelId = "Default";
+        val builder: NotificationCompat.Builder = NotificationCompat.Builder(this, channelId)
+            .setSmallIcon(R.mipmap.ic_launcher)
+            .setContentTitle(remoteMessage.notification?.title)
+            .setContentText(remoteMessage.notification?.body)
+            .setAutoCancel(true)
+            .setContentIntent(pendingIntent)
+        val manager: NotificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel =  NotificationChannel(channelId, "$channelId channel", NotificationManager.IMPORTANCE_DEFAULT);
+            manager.createNotificationChannel(channel);
 
-//    @SuppressLint("StringFormatInvalid")
-//    private fun sendRegistrationToServer(token: String) {
-//        val msg = resources.getString(R.string.msg_token_fmt, token)
-//        Log.d(TAG, msg)
-//        Toast.makeText(baseContext, msg, Toast.LENGTH_SHORT).show()
+        }
+            manager.notify(0, builder.build());
+
+//            if (true) {
+//                scheduleJob()
+//            } else {
+//                handleNow()
+//            }
+        }
+//        remoteMessage.notification?.let {
+//            Log.d(TAG, "Message Notification Body: ${it.body}")
+//        }
+//    }
+//    private fun scheduleJob() {
+//    }
+//    private fun handleNow() {
 //    }
 }
+
+
